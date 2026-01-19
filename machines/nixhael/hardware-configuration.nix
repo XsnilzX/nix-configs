7d8032dbc6923ba89a8ae4c61c8ffbd9ff56f146
Richard Taesler <officiall.xdragonyt@gmail.com>
2026-01-19 16:19:18 +0100

    updated hardware config

--- machines/nixhael/hardware-configuration.nix
+++ machines/nixhael/hardware-configuration.nix
@@ -12,36 +12,41 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
+  console.keyMap = "de";
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/6c79bd41-9907-45c3-93ed-cc5cf9cbbb8e";
-    fsType = "ext4";
+    device = "/dev/disk/by-uuid/29fa8ffb-b933-45b1-98cd-6152dfa0a3c4";
+    fsType = "btrfs";
+    options = ["subvol=@" "compress=zstd:4" "noatime" "ssd" "discard=async" "space_cache=v2"];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/29fa8ffb-b933-45b1-98cd-6152dfa0a3c4";
+    fsType = "btrfs";
+    options = ["subvol=@home" "compress=zstd:4" "noatime" "ssd" "discard=async" "space_cache=v2"];
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/29fa8ffb-b933-45b1-98cd-6152dfa0a3c4";
+    fsType = "btrfs";
+    options = ["subvol=@nix" "noatime" "nodatacow" "ssd" "discard=async" "space_cache=v2"];
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/F00C-D53B";
+    device = "/dev/disk/by-uuid/3F85-4835";
     fsType = "vfat";
     options = ["fmask=0022" "dmask=0022"];
   };
 
   swapDevices = [
-    {device = "/dev/disk/by-uuid/be5baaab-28e6-4124-9b55-5c78cf911e04";}
+    {device = "/dev/disk/by-uuid/9f6e0323-023a-40d8-9f48-5927d9d652a8";}
   ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp11s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-
   hardware = {
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     openrazer = {
